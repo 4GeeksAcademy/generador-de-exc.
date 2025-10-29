5371e17ea2c4fc26055c6e74553ba3951880f0a4
sarah-irc <sarahcardoso.sc@gmail.com>
2025-10-29 10:53:06 +0000

    done generador

--- src/app.js
+++ src/app.js
@@ -9,3 +9,20 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+//my code from here
+let who = ['My mom', 'My father', 'My boyfriend', 'My teacher'];
+let action = ['broke', 'forgot', 'lost', 'threw away']; 
+let what = ['my keys', 'my food', 'my homework'];
+let when = ['last week', 'while I was playing', 'during my lunch', 'when I was sleeping'];
+let feeling = ['and I am sad', 'and I am happy', 'and I am angry', 'and I am relieved'];
+
+const randomNumber = arr => {
+  return Math.floor(Math.random() * arr.length)
+}
+
+console.log(randomNumber(who))
+console.log(randomNumber(action))
+console.log(randomNumber(what))
+console.log(randomNumber(when))
+console.log(randomNumber(feeling))
